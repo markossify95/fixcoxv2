613d98da89d2d9a26541bd9ed42ed41e7e5ee5b1
Dule <Dule@Dule-PC>
2016-03-21 15:44:09 +0100

    Par formi i kuraca

---
+++ src/gui/komponente/GuiKontrole.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui.komponente;
+
+/**
+ *
+ * @author Dule
+ */
+public class GuiKontrole {
+    public static void addPicture(){
+    
+    }
+}
